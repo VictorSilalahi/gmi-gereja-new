33d9f3e1df8b84c64bee0ee6c44474e0cd08be0d
Victor Silalahi <victorbiz766hi@gmail.com>
2026-07-08 20:54:18 +0700

    modified:   app/Controllers/Webapi/V1/Distrik/Jemaat.php

--- app/Controllers/Webapi/V1/Distrik/Jemaat.php
+++ app/Controllers/Webapi/V1/Distrik/Jemaat.php
@@ -67,23 +67,146 @@ class Jemaat extends BaseController
                     $gol_AB = 0;
                     $gol_O = 0;
 
+                    $SD = 0;
+                    $SMP = 0;
+                    $SMA_SMK = 0;
+                    $D3 = 0;
+                    $S1 = 0;
+                    $S2 = 0;
+                    $S3 = 0;
+                    $P_None = 0;
+
                     $ASN = 0;
-                    
+                    $TNI_Polri = 0;
+                    $Karyawan_Swasta = 0;
+                    $Pedagang = 0;
+                    $Wiraswasta = 0;
+                    $Dokter = 0;
+                    $Petani = 0;
+                    $Pek_None = 0;
+
+
+
                     foreach($result as $row) {
 
                         // golongan darah
-                        if ($row)
+                        if ($row->golongan_darah == 'A') {
+                            $gol_A = $gol_A + 1;
+                        }
+
+                        if ($row->golongan_darah == 'B') {
+                            $gol_B = $gol_B + 1;
+                        }
+
+                        if ($row->golongan_darah == 'AB') {
+                            $gol_AB = $gol_AB + 1;
+                        }
+
+                        if ($row->golongan_darah == 'O') {
+                            $gol_O = $gol_O + 1;
+                        }
+
+
+                        // pendidikan terakhir
+                        if ($row->pendidikan_terakhir == 'SD') {
+                            $SD = $SD + 1;
+                        }
+
+                        if ($row->pendidikan_terakhir == 'SMP') {
+                            $SMP = $SMP + 1;
+                        }
+
+                        if ($row->pendidikan_terakhir == 'SMA=SMK') {
+                            $SMA_SMK = $SMA_SMK + 1;
+                        }
+
+                        if ($row->pendidikan_terakhir == 'D3') {
+                            $D3 = $D3 + 1;
+                        }
+
+                        if ($row->pendidikan_terakhir == 'S1') {
+                            $S1 = $S1 + 1;
+                        }
+
+                        if ($row->pendidikan_terakhir == 'S2') {
+                            $S2 = $S2 + 1;
+                        }
+
+                        if ($row->pendidikan_terakhir == 'S3') {
+                            $S3 = $S3 + 1;
+                        }
+
+                        if ($row->pendidikan_terakhir == 'None') {
+                            $P_None = $P_None + 1;
+                        }
+
+
+                        // pekerjaan
+                        if ($row->pekerjaan == 'ASN') {
+                            $ASN = $ASN + 1;
+                        }
+
+                        if ($row->pekerjaan == 'TNI-Polri') {
+                            $TNI_Polri = $TNI_Polri + 1;
+                        }
+
+                        if ($row->pekerjaan == 'Karyawan-Swasta') {
+                            $Karyawan_Swasta = $Karyawan_Swasta + 1;
+                        }
+
+                        if ($row->pekerjaan == 'Pedagang') {
+                            $Pedagang = $Pedagang + 1;
+                        }
+
+                        if ($row->pekerjaan == 'Wiraswasta') {
+                            $Wiraswasta = $Wiraswasta + 1;
+                        }
+                    
+                        if ($row->pekerjaan == 'Dokter') {
+                            $Dokter = $Dokter + 1;
+                        }
+
+                        if ($row->pekerjaan == 'Petani') {
+                            $Petani = $Petani + 1;
+                        }
+
+                        if ($row->pekerjaan == 'None') {
+                            $Pek_None = $Pek_None + 1;
+                        }   
 
                     }
 
                 }
-                // golongan darah
+
 
 
             }
             
             $data['jumlah_kk'] = $jumlah_kk;
             $data['jumlah_jiwa'] = $jumlah_jiwa;
+
+            $data['gol_darah']['A'] = $gol_A;
+            $data['gol_darah']['B'] = $gol_B;
+            $data['gol_darah']['AB'] = $gol_AB;
+            $data['gol_darah']['O'] = $gol_O;
+
+            $data['pendidikan']['SD'] = $SD;
+            $data['pendidikan']['SMP'] = $SMP;
+            $data['pendidikan']['SMA_SMK'] = $SMA_SMK;
+            $data['pendidikan']['D3'] = $D3;
+            $data['pendidikan']['S1'] = $S1;
+            $data['pendidikan']['S2'] = $S2;
+            $data['pendidikan']['S3'] = $S3;
+            $data['pendidikan']['None'] = $P_None;
+
+            $data['pekerjaan']['ASN'] = $ASN;
+            $data['pekerjaan']['TNI-Polri'] = $TNI_Polri;
+            $data['pekerjaan']['Karyawan-Swasta'] = $Karyawan_Swasta;
+            $data['pekerjaan']['Pedagang'] = $Pedagang;
+            $data['pekerjaan']['Wiraswasta'] = $Wiraswasta;
+            $data['pekerjaan']['Dokter'] = $Dokter;
+            $data['pekerjaan']['Petani'] = $Petani;
+            $data['pekerjaan']['None'] = $Pek_None;
 
 
             return $this->respond([
