9f076a56842d80cdccfda9f6cfc74a18bac6dd98
Victor Silalahi <victorbiz766hi@gmail.com>
2026-07-31 20:30:43 +0700

    renamed:    env-local.example -> env.local 	renamed:    env-server.example -> env.server 	modified:   gmi-member.sql

--- gmi-member.sql
+++ gmi-member.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 31, 2026 at 06:23 AM
--- Server version: 10.4.32-MariaDB
+-- Generation Time: Jul 31, 2026 at 03:28 PM
+-- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -71,7 +71,8 @@ CREATE TABLE `tgereja` (
 --
 
 INSERT INTO `tgereja` (`gereja_id`, `distrik`, `email`, `password`, `nama_gereja`, `alamat`, `lat`, `lng`, `kondisi_bangunan`, `kepemilikan`, `db_id`, `identity_link`, `path_sk`, `created_at`, `updated_at`) VALUES
-('9beaf151-63ee-4d4a-8520-8918f74db30b', 'D-II', 'silalahitotok@gmail.com', 'testing', 'GMI Kasih Karunia', 'Jl Hang Tuah', '', '', 'Permanen', 'Milik Sendiri', 'g-dnja1q', 'QBPh9aPWLoUv', 'public/uploads/sk/1781845451_629fb7490b55adac04f9.jpg', '2026-06-19', '2026-06-19');
+('9beaf151-63ee-4d4a-8520-8918f74db30b', 'D-II', 'silalahitotok@gmail.com', 'testing', 'GMI Kasih Karunia', 'Jl Hang Tuah', '3.5441744', '98.5976251', 'Permanen', 'Milik Sendiri', 'g-f9kyfz', 'QBPh9aPWLoUv', 'public/uploads/sk/1781845451_629fb7490b55adac04f9.jpg', '2026-06-19', '2026-06-19'),
+('68151118-acfb-4285-9e01-681d56c2045e', 'D-II', 'victorbiz766hi@gmail.com', 'yW9YimFG', 'GMI XXX', 'Jl Madong Lubis no 9 Medan', '3.591858738825689', '98.67233008146287', 'Semi Permanen', 'Milik Sendiri', 'g-bqlgl2', 'EWu8KxyNsR2b', 'public/uploads/sk/1785480477_af130d7a021e720fe9f5.jpg', '2026-07-31', '2026-07-31');
 
 -- --------------------------------------------------------
 
@@ -90,7 +91,8 @@ CREATE TABLE `tpendeta` (
 --
 
 INSERT INTO `tpendeta` (`pendeta_id`, `nama`, `email`) VALUES
-(3, 'Pdt Lubis', 'silalahitotok@gmail.com');
+(3, 'Pdt Lubis', 'silalahitotok@gmail.com'),
+(4, 'Pdt Lubis', 'jonathan@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -122,7 +124,7 @@ ALTER TABLE `tdistrik`
 -- AUTO_INCREMENT for table `tpendeta`
 --
 ALTER TABLE `tpendeta`
-  MODIFY `pendeta_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `pendeta_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
