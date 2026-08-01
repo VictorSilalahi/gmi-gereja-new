00a514e2e07e7caa7fe5557c8e30f3cfea66a398
Victor Silalahi <victorbiz766hi@gmail.com>
2026-08-01 18:27:49 +0700

    modified:   assets/js/pendaftaran/pendaftaran.js

--- assets/js/pendaftaran/pendaftaran.js
+++ assets/js/pendaftaran/pendaftaran.js
@@ -233,7 +233,11 @@ function success(position) {
     long = position.coords.longitude;
 
     let map = L.map('map').setView([lat, long], 13);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //     maxZoom: 19,
+    //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    // }).addTo(map);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
