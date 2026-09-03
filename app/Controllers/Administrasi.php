a4af94afcc656356d5bf9feea1b1670b0854d776
Victor Silalahi <victorbiz766hi@gmail.com>
2026-09-03 10:57:45 +0700

    modified:   app/Controllers/Administrasi.php

--- app/Controllers/Administrasi.php
+++ app/Controllers/Administrasi.php
@@ -16,13 +16,13 @@ class Administrasi extends BaseController
 
     public function index()
     {
-        // return view('administrasi/login');
-        return view('administrasi/under_construction');        
+        return view('administrasi/login');
+        // return view('administrasi/under_construction');        
     }
 
     public function index2()
     {
-        return view('administrasi/login');
+        // return view('administrasi/login');
     }
 
     public function logout()
